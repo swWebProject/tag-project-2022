3d9e25c0020d04b6b30a795dc4bf889c6c5ae888
DongminL <milkskfk12@gmail.com>
2022-07-21 17:14:10 +0900

    create

---
+++ src/main/java/com/example/SWExhibition/SwExhibitionApplication.java
@@ -0,0 +1,13 @@
+package com.example.SWExhibition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwExhibitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwExhibitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SWExhibition/SwExhibitionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SWExhibition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwExhibitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
