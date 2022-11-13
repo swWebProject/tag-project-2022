e5f88bf06bb170f4b41c5f2ca0f04ffa8a5187c7
Jihyeon-Park <qkrwlgus20020@gmail.com>
2022-11-14 02:06:50 +0900

    영화 제목으로 검색

--- src/main/java/com/example/SWExhibition/repository/MoviesRepository.java
+++ src/main/java/com/example/SWExhibition/repository/MoviesRepository.java
@@ -4,8 +4,13 @@ import com.example.SWExhibition.entity.Movies;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MoviesRepository extends JpaRepository<Movies, Long> {
     boolean existsByMovieCd(String movieCd);    // DB에 해당 영화 코드가 있는지 검사
     Movies findByMovieCd(String movieCd); // DB에서 MovieCd로 데이터 불러오기
+
+    List<Movies> findByTitle(String keyword);
+
 }
