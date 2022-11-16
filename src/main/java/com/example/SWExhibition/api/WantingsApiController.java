75ebe4119e866c4399fc7d58b4f5acc0ca523416
DongminL <milkskfk12@gmail.com>
2022-11-16 12:29:59 +0900

    보고 싶은 영화 api

--- src/main/java/com/example/SWExhibition/api/WantingsApiController.java
+++ src/main/java/com/example/SWExhibition/api/WantingsApiController.java
@@ -24,11 +24,12 @@ public class WantingsApiController {
     // 보고 싶은 영화 추가
     @PostMapping("/api/post/wanting")
     public ResponseEntity<Wantings> postWanting(@AuthenticationPrincipal PrincipalDetails principalDetails, @RequestBody WantingsDto dto) {
-        Wantings wanting = wantingsService.updateWanting(principalDetails, dto);
+        Wantings wanting = wantingsService.updateWanting(principalDetails, dto);    // 세션 정보와 보내준 값으로 보고 싶은 영화 업데이트 ( 추가 또는 삭제)
 
         return ResponseEntity.status(HttpStatus.OK).body(wanting);
     }
 
+    // 유저의 보고 싶은 영화 목록
     @GetMapping("/api/get/wanting")
     public List<Wantings> getWantingList(@AuthenticationPrincipal PrincipalDetails principalDetails) {
         List<Wantings> wantingsList = wantingsService.showWantings(principalDetails);   // 보고 싶은 영화 목록
