7b09c5704be66fa2b5c6afb0d2d2acc6a203680e
Jihyeon-Park <qkrwlgus20020@gmail.com>
2022-11-16 18:20:27 +0900

    build: 좋아요 엔티티

---
+++ src/main/java/com/example/SWExhibition/entity/Likes.java
@@ -0,0 +1,34 @@
+package com.example.SWExhibition.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter @Setter
+public class Likes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long likeId;
+
+    @ManyToOne
+    @JoinColumn(name = "users_Id")
+    private Users userId;
+
+    @ManyToOne
+    @JoinColumn(name = "comments_commentId")
+    private Comments comments;
+
+    @Builder
+    public Likes(Long likeId, Users userId, Comments comments) {
+        this.likeId = likeId;
+        this.userId = userId;
+        this.comments = comments;
+    }
+
+}
