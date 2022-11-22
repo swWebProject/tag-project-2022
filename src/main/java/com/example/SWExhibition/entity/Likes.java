b0d0a960753728fd575be0f888fd98de7119a560
Jihyeon-Park <qkrwlgus20020@gmail.com>
2022-11-22 19:25:27 +0900

    Like 테이블  PrincipalDetails users 변경

--- src/main/java/com/example/SWExhibition/entity/Likes.java
+++ src/main/java/com/example/SWExhibition/entity/Likes.java
@@ -1,36 +1,31 @@
 package com.example.SWExhibition.entity;
 
-import lombok.Builder;
+import com.example.SWExhibition.security.PrincipalDetails;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
-@Entity
 @NoArgsConstructor
 @Getter @Setter
+@AllArgsConstructor
+@Table(name = "likes")
+@Entity
 public class Likes {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long likeId;
-
-    //좋야요 한 유저 아이디
-    @ManyToOne
-    @JoinColumn(name = "users_Id")
-    private Users userId;
-
-    //좋아요 한 댓글
-    @ManyToOne
-    @JoinColumn(name = "comments_commentId")
+    @ManyToOne(fetch = FetchType.LAZY)
     private Comments comments;
 
-    @Builder
-    public Likes(Long likeId, Users userId, Comments comments) {
-        this.likeId = likeId;
-        this.userId = userId;
-        this.comments = comments;
-    }
+    private PrincipalDetails users;
 
+
+    public Likes(Comments comments, PrincipalDetails users) {
+        this.comments = comments;
+        this.users = users;
+
+    }
 }
